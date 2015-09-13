3e1a9dad10ab0202a73c27d4d2970ff3e55512af
Stacy Huang <syhhuang88@gmail.com>
2015-09-12 18:48:17 -0700

    Set up heading and footer

--- client/main/mainCtrl.js
+++ client/main/mainCtrl.js
@@ -4,9 +4,19 @@
 
 		.controller('MainCtrl', MainCtrl);
 
-		MainCtrl.$inject = ['$scope'];
+		MainCtrl.$inject = ['$scope', 'MainFactory'];
 
-		function MainCtrl($scope){
+		function MainCtrl($scope, MainFactory){
+			$scope.word = [];
 
+			$scope.getWord = function(){
+				MainFactory.getWord()
+					.then(function(word){
+						console.log(word);
+						$scope.word = word;
+					});
+			}
+
+			$scope.getWord();
 		}
 })();

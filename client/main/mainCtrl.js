f6783d5c6686af6dc8675d1a75422cfee01d33b7
Stacy Huang <syhhuang88@gmail.com>
2015-09-12 19:00:20 -0700

    Set up MainFactory to query random word from the wordnik api

--- client/main/mainCtrl.js
+++ client/main/mainCtrl.js
@@ -8,14 +8,23 @@
 
 		function MainCtrl($scope, MainFactory){
 			$scope.word = [];
+			$scope.characters = [];
 
 			$scope.getWord = function(){
 				MainFactory.getWord()
 					.then(function(word){
-						console.log(word);
-						$scope.word = word;
+						$scope.word = word.word;
+						$scope.characters = [];
+						for(var i=0; i<$scope.word.length; i++){
+							$scope.characters.push($scope.word[i]);
+						}
+						console.log($scope.characters);
 					});
-			}
+			};
+
+			$scope.parseWord = function(){
+
+			};
 
 			$scope.getWord();
 		}
--- client/main/mainService.js
+++ client/main/mainService.js
@@ -7,6 +7,18 @@
 		MainFactory.$inject = ['$http'];
 
 		function MainFactory($http){
+			var getWord = function(){
+				return $http({
+					method: 'GET',
+					url: 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+				})
+				.then(function(res){
+					return res.data;
+				})
+			};
 
+			return {
+				getWord: getWord
+			};
 		}
 })();

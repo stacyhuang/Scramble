e93bc7cda0c3235bfbf32d10e5d281d84c9619d3
Stacy Huang <syhhuang88@gmail.com>
2015-09-12 19:09:02 -0700

    Display word as individual character

--- client/main/mainService.js
+++ client/main/mainService.js
@@ -10,7 +10,7 @@
 			var getWord = function(){
 				return $http({
 					method: 'GET',
-					url: 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+					url: 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=6&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
 				})
 				.then(function(res){
 					return res.data;
